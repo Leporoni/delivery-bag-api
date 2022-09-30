3170d095de24e91d28e782b61e2f78866d18d02b
Leporoni <leporoni@hotmail.com>
2022-09-30 15:50:26 -0300

    first commit

---
+++ src/main/java/com/leporonitech/deliverybag/DeliveryBagApiApplication.java
@@ -0,0 +1,13 @@
+package com.leporonitech.deliverybag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryBagApiApplication.class, args);
+	}
+
+}
